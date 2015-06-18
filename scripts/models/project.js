7faef13e68886eefedc77fdce20390bb537bf524
Austin <austineady@github.com>
2015-06-18 12:04:14 -0400

    Working on Shopping Cart

---
+++ scripts/models/project.js
@@ -0,0 +1,40 @@
+import {DrinkCollection} from './drinkcollection';
+
+export default Backbone.Model.extend({
+
+  idAttribute: 'objectId',
+
+  urlRoot: 'https://api.parse.com/1/Project',
+
+  defaults: {
+    name: ''
+  },
+
+  initialize: function() {
+    // Create a drink collection to keep track of
+    this.drinks = new DrinkCollection();
+
+    // Trigger all tasks events on myself
+    this.listenTo(this.drinks, 'all', this.trigger.bind(this));
+  },
+
+  add: function(models, options) {
+    this.drinks.add(models, options);
+  },
+
+  remove: function(models, options) {
+    this.drinks.remove(models, options);
+  },
+
+  toJSON: function() {
+    return _.extend({}, this.attributes, {
+      drinks: this.drinks.map(function(drink) {
+        return {
+          "__type": "Pointer",
+          "clasName": "Drinks",
+          "objectId": drink.id
+        };
+      })
+    });
+  }
+});
